74c9342d025cc92c429eae1f9354012937129ad2
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-22 10:29:22 +0100

    Completed the first part of the second snack

---
+++ snack-2/js/script.js
@@ -0,0 +1,41 @@
+/***
+Chiedere all'API 10 nomi da inserire in un array di invitati.
+ Una volta generata la lista chiedere all'utente di dire il proprio nome.
+ Se è uno degli invitati ritornare un messaggio di benvenuto, altrimenti di accesso negato.
+*/
+
+const {createApp} = Vue;
+
+
+createApp({
+    data(){
+        return{
+            randomGuestsList : [],
+            totalGuests : 10,
+        }
+    },
+
+
+
+    methods :{
+        getARandomName(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/name')
+                .then(response => {
+                    this.randomGuestsList.push(response.data.response);
+                })
+                .catch(error => {
+                    console.warn(error.response.data.message)
+                })
+        },
+
+        getXRandomNames(){
+            for(let i = 0 ; i<this.totalGuests ; i++){
+                this.getARandomName();
+            }
+        },
+    },
+
+    created(){
+        this.getXRandomNames();
+    }
+}).mount('#app')
